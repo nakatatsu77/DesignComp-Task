09e31c63663dd2610489b8ab6fb6dfb52f233223
nakatatsu77 <nakatatsu1177@gmail.com>
2023-11-12 22:09:41 +0900

    修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { Footer } from "@/components/footer/Footer";
 import "@/styles/globals.css";
 import "@/components/service/Service.css";
 import "@/components/news/News.css";
-import "@/components/contact/Contact.css";
 import "@/components/header/Header.css";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/header/Header";
